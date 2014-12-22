ff895aad4c580a1021709d1bbcb3346e4138eb3d
Garry Cairns <garryjcairns@gmail.com>
2014-12-22 20:02:27 +0000

    Part one exercise two complete and working.

--- upenn/02.hs
+++ upenn/02.hs
@@ -4,9 +4,18 @@ module LogAnalysis where
 import Log
 
 parseMessage :: String -> LogMessage
-parseMessage _ = Unknown "Hi"
+parseMessage xs
+  | ws!!0 == "I" = LogMessage Info (read (ws!!1)) (unwords $ drop 2 ws)
+  | ws!!0 == "W" = LogMessage Warning (read (ws!!1)) (unwords $ drop 2 ws)
+  | ws!!0 == "E" = LogMessage (Error (read (ws!!1))) (read (ws!!2)) (unwords $ drop 2 ws)
+  | otherwise = Unknown xs
+  where ws = words xs
 
 parse :: String -> [LogMessage]
+parse xs = map parseMessage $ lines xs
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert _ _ = MessageTree
 
 main :: IO()
 main = do
