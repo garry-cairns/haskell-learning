9a28131f98448cbc71459d1de3eb07b367b80a52
Garry Cairns <garryjcairns@gmail.com>
2015-01-18 22:47:02 +0000

    Completed second exercise.

--- upenn/02.hs
+++ upenn/02.hs
@@ -5,9 +5,9 @@ import Log
 
 parseMessage :: String -> LogMessage
 parseMessage xs
-  | ws!!0 == "I" = LogMessage Info (read (ws!!1)) (unwords $ drop 2 ws)
-  | ws!!0 == "W" = LogMessage Warning (read (ws!!1)) (unwords $ drop 2 ws)
-  | ws!!0 == "E" = LogMessage (Error (read (ws!!1))) (read (ws!!2)) (unwords $ drop 2 ws)
+  | head ws == "I" = LogMessage Info (read (ws!!1)) (unwords $ drop 2 ws)
+  | head ws == "W" = LogMessage Warning (read (ws!!1)) (unwords $ drop 2 ws)
+  | head ws == "E" = LogMessage (Error (read (ws!!1))) (read (ws!!2)) (unwords $ drop 2 ws)
   | otherwise = Unknown xs
   where ws = words xs
 
@@ -15,9 +15,25 @@ parse :: String -> [LogMessage]
 parse xs = map parseMessage $ lines xs
 
 insert :: LogMessage -> MessageTree -> MessageTree
-insert _ _ = MessageTree
+insert (Unknown _) messageTree = messageTree
+insert x Leaf = Node Leaf x Leaf
+insert x@(LogMessage _ timestamp _) (Node leftChild y@(LogMessage _ u _) rightChild)
+  | timestamp > u = Node leftChild y (insert x rightChild)
+  | otherwise = Node (insert x leftChild) y rightChild
 
-main :: IO()
-main = do
-    print $ parseMessage "I 29 la la la" == LogMessage Info 29 "la la la"
-    print $ parseMessage "This is not the right format" == Unknown "This is not the right format"
+build :: [LogMessage] -> MessageTree
+build (x:xs) = insert x (build xs)
+build [] = Leaf
+--
+inOrder :: MessageTree -> [LogMessage]
+inOrder (Node leftChild@(Node _ _ _) _ _) = inOrder leftChild
+inOrder (Node Leaf x rightChild@(Node _ _ _)) = x:inOrder rightChild
+inOrder (Node Leaf x Leaf) = [x]
+inOrder Leaf = []
+--
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong (l@(LogMessage (Error n) _ s):ls)
+  | n > 50 = s:whatWentWrong ls
+  | otherwise = whatWentWrong ls
+whatWentWrong (l:ls) = whatWentWrong ls
+whatWentWrong [] = []
