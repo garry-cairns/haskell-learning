905ec69892a8f5ccb7b41b4c1112590549edc848
Garry Cairns <garryjcairns@gmail.com>
2015-01-27 21:19:11 +0000

    Assignment 3 complete

--- upenn/Golf.hs
+++ upenn/Golf.hs
@@ -2,24 +2,57 @@ module Golf where
 
     import Data.List
 
+    -- Exercise 1
+    -- Take a list xs and an integer n, and return a list of every nth element of xs
     everyNth :: [a] -> Int -> [a]
     everyNth xs n = [snd x | x <- (zip [1..] xs), fst x `mod` n == 0]
 
     skips :: [a] -> [[a]]
     skips xs = map (everyNth xs) [1..length xs]
 
+    -- Exercise 2
+    -- Take a list of integers and produce a list of local maxima
     triples :: [a] -> [[a]]
     triples (x:xs)
       | length (x:xs) < 3 = []
       | otherwise = (x:take 2 xs) : triples xs
 
-    isMiddleMax :: Ord a => [a] -> Bool
+    isMiddleMax :: (Ord a) => [a] -> Bool
     isMiddleMax (x:y:z:[]) = x < y && y > z
 
     localMaxima :: [Integer] -> [Integer]
     localMaxima xs = map maximum . filter isMiddleMax $ triples xs
 
-    histogram :: [Integer] -> String
+    -- Exercise 3
+    -- Take a list of integers 0-9 and return a histogram
+    groupSort :: (Eq a, Ord a) => [a] -> [[a]]
+    groupSort xs = group $ sort xs
+
+    countInstance :: (Eq a, Ord a) => [a] -> (a, Int)
+    countInstance xs = (head xs, length xs)
+
+    countInstances :: (Eq a, Ord a) => [a] -> [(a, Int)]
+    countInstances xs = map (countInstance) $ groupSort xs
+
+    maxInstances :: [Int] -> Int
+    maxInstances xs = maximum $ map (snd) $ countInstances xs
+
+    spaceOrStar :: [Int] -> Int-> Char
+    spaceOrStar xs y
+      | y `elem` xs = '*'
+      | otherwise = ' '
+
+    getData :: (Eq a, Ord a) => [a] -> Int -> [a]
+    getData xs n = [fst x | x <- countInstances xs, snd x >= n]
+
+    histRow :: [Int] -> String
+    histRow xs = map (spaceOrStar xs) [0..9]
+
+    buildHist :: [Int] -> String
+    buildHist xs = intercalate "\n" . map (histRow) $ map (getData xs) . reverse $ [1..maxInstances xs]
+
+    histogram :: [Int] -> String
+    histogram xs = buildHist xs ++ "\n==========\n0123456789\n"
 
     main :: IO()
     main = do
@@ -31,3 +64,4 @@ module Golf where
         print $ localMaxima [2,9,5,6,1]
         print $ localMaxima [2,3,4,1,5]
         print $ localMaxima [1,2,3,4,5]
+        putStr $ histogram [1,4,5,4,6,6,3,4,2,4,9]
